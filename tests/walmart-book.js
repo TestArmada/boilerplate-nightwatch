9ff6c1662720b762773c6bb13b6e61df41ba5a46
Abha Gupta <agupta3@walmart.com>
2015-09-16 11:32:09 -0700

    capturing performance metrics in a command

--- tests/walmart-book.js
+++ tests/walmart-book.js
@@ -1,11 +1,11 @@
 var Test = require("../lib/example-base-test-class");
 
 module.exports = new Test({
-
-  "Search for Sam Walton Book": function (client) {
+  "Search for Sam Walton Book- Again": function (client) {
     client
       .resizeWindow(1280, 1024)
       .url("http://www.walmart.com/search/?query=sam%20walton%20made%20in%20america")
+      .getPerformance()
   },
 
   "Check product description": function (client) {
@@ -13,6 +13,5 @@ module.exports = new Test({
       .assert.elContainsText("[data-item-id='403453'] .tile-heading", "My Story")
       .assert.elContainsText("[data-item-id='403453'] .media-details", "Paperback")
   }
-
 
 });
