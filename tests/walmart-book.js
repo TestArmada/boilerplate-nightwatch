587db64fb1fa5793791dbbac5561927cbfae9840
Maciej Adwent <MAdwent@walmartlabs.com>
2015-09-16 17:19:15 -0700

    PoC work for performance metrics via a reporter

--- tests/walmart-book.js
+++ tests/walmart-book.js
@@ -1,11 +1,20 @@
 var Test = require("../lib/example-base-test-class");
 
+var url = "http://www.walmart.com/search/?query=sam%20walton%20made%20in%20america";
+
 module.exports = new Test({
   "Search for Sam Walton Book- Again": function (client) {
     client
       .resizeWindow(1280, 1024)
-      .url("http://www.walmart.com/search/?query=sam%20walton%20made%20in%20america")
-      .getPerformance()
+      .url(url)
+      .getPerformance(function (result) {
+        // Send a message to Magellan about this page
+        process.send({
+          type: "performance-metrics",
+          url: url,
+          metrics: result
+        });
+      })
   },
 
   "Check product description": function (client) {
