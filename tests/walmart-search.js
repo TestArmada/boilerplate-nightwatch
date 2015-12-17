f7ab06903bb1f615afc561ebbc7cf09df0467aaf
Lei Zhu <lzhu@walmart.com>
2015-12-16 18:09:45 -0800

    example of page object via walmart search

---
+++ tests/walmart-search.js
@@ -0,0 +1,38 @@
+var Test = require("../lib/example-base-test-class");
+
+module.exports = new Test({
+  "Load walmart home page": function (client) {
+    var hp = client.page["wm-homepage"]();
+    hp
+    // open url once
+      .navigate()
+      .assert.title('Walmart.com: Save money. Live better.')
+  },
+
+  "Search for tv": function (client) {
+    var hp = client.page["wm-homepage"]();
+    // no need to call `navigate`
+    hp
+      .search('tv');
+  },
+
+  "Jump to first item listed on search result page": function (client) {
+    client.page["wm-search"]()
+      .checkFirstItem();
+  },
+
+  "Verify item detail on item page": function (client) {
+    var ip = client.page["wm-itempage"]();
+
+    ip.section.buyingTable
+      .assert.containsText('@mainPrice', '$159');
+
+    ip.section.aboutProduct
+      .assert.containsText('@specification', 'Specifications')
+      .waitForElementVisible('@specification')
+      .assert.containsText('@specification', 'Remote Included: Yes');
+
+    client.end();
+  }
+
+});
