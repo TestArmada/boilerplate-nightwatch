93efc81245d1048308e9100d7b57abbcb4da7bf8
Lei Zhu <lzhu@walmart.com>
2015-12-30 14:21:22 -0800

    add data file

---
+++ conf/data/local.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "drydock": {
+    "tokyo": {
+      "name": "Tokyo"
+    },
+    "europe": "Europe",
+    "beijing": {
+      "name": "Beijing",
+      "country": "China",
+      "description": "It is the most populous city in the China"
+    },
+    "timestamp": function () {
+      return new Date().getTime();
+    }
+  }
+}
--- tests/demo-page-object.js
+++ tests/demo-page-object.js
@@ -1,39 +1,46 @@
 var Test = require("../lib/example-base-test-class");
+var td = require("dpro").drydock;
 
-module.exports = new Test({
-  "Load demo first page": function (client) {
-    var df = client.page["demo-first"]();
+module.exports =
+  new Test(
+    [td.tokyo, td.europe, td.beijing],
+    function (tokyo, europe, beijing) {
+      return {
+        "Load demo first page": function (client) {
+          var df = client.page["demo-first"]();
 
-    df
-    // open url once
-      .navigate()
-      .api.resizeWindow(1200, 800);
-  },
+          df
+          // open url once
+            .navigate()
+            .api.resizeWindow(1200, 800);
+        },
 
-  "Verify all cities on first page": function (client) {
-    var df = client.page["demo-first"]();
-    // no need to call `navigate`
-    df
-      .assert.elContainsText("#tokyo", "Tokyo")
-      .assert.elContainsText(".city:eq(1) p:eq(1)", "Europe");
-  },
+        "Verify all cities on first page": function (client) {
+          var df = client.page["demo-first"]();
+          // no need to call `navigate`
+          df
+            .assert.elContainsText("#tokyo", tokyo.name)
+            .assert.elContainsText(".city:eq(1) p:eq(1)", europe);
+        },
 
-  "Jump to demo second page": function (client) {
-    client
-      .page["demo-first"]()
-      .jumpToSecondDemo();
-  },
+        "Jump to demo second page": function (client) {
+          client
+            .page["demo-first"]()
+            .jumpToSecondDemo();
+        },
 
-  "Verify cities on second page": function (client) {
-    var ds = client.page["demo-second"]();
+        "Verify cities on second page": function (client) {
+          var ds = client.page["demo-second"]();
 
-    ds.section.beijing
-      .waitForElementVisible('@title')
-      .assert.containsText("@title", "Beijing")
-      .assert.containsText("@description", "China")
-      .assert.containsText("@content", "It is the most populous city in the China");
+          ds.section.beijing
+            .waitForElementVisible('@title')
+            .assert.containsText("@title", beijing.name)
+            .assert.containsText("@description", beijing.country)
+            .assert.containsText("@content", beijing.description);
 
-    client.end();
-  }
+          client.end();
+        }
 
-});
+      }
+    }
+  );
