5d012c16bf12f8c214777074716f9078a6e00a3d
Lei Zhu <lzhu@walmart.com>
2016-04-10 14:34:52 -0700

    add nightwatch-extra and dpro

--- tests/demo-page-object.js
+++ tests/demo-page-object.js
@@ -1,7 +1,8 @@
 var Test = require("../lib/example-base-test-class");
+var dpro = require("dpro");
 
 module.exports = new Test({
-  "Load demo first page": function (client) {
+  "Load demo first page": function(client) {
     var df = client.page["demo-first"]();
 
     df
@@ -10,7 +11,7 @@ module.exports = new Test({
       .api.resizeWindow(1200, 800);
   },
 
-  "Verify all cities on first page": function (client) {
+  "Verify all cities on first page": function(client) {
     var df = client.page["demo-first"]();
     // no need to call `navigate`
     df
@@ -18,22 +19,47 @@ module.exports = new Test({
       .assert.elContainsText(".city:eq(1) p:eq(1)", "Europe");
   },
 
-  "Jump to demo second page": function (client) {
+  "Jump to demo second page": function(client) {
     client
       .page["demo-first"]()
       .jumpToSecondDemo();
   },
 
-  "Verify cities on second page": function (client) {
+  "Verify cities on second page": function(client) {
     var ds = client.page["demo-second"]();
 
     ds.section.beijing
       .getEl('@title')
-      .assert.elContainsText("@title", "Beijing")
+      .assert.elContainsText("#beijing", "Beijing")
       .assert.elContainsText("@description", "China")
       .assert.elContainsText("@content", "It is the most populous city in the China");
+  },
 
-    client.end();
+  "Verify cities on second page - performance improvement": function(client) {
+    var ds = client.page["demo-second"]();
+    var beijing = ds.section.beijing;
+
+    beijing
+      .getEl(beijing.elements.title.selector)
+      .assert.elContainsText(beijing.elements.title.selector,
+        "Beijing")
+      .assert.elContainsText(beijing.elements.description.selector,
+        "China")
+      .assert.elContainsText(beijing.elements.content.selector,
+        "It is the most populous city in the China");
+  },
+
+  "Verify cities on second page - maintenance improvement": function(client) {
+    var ds = client.page["demo-second"]();
+    var beijing = ds.section.beijing;
+    
+    beijing
+      .getEl(beijing.elements.title.selector)
+      .assert.elContainsText(beijing.elements.title.selector,
+        dpro.beijing.title)
+      .assert.elContainsText(beijing.elements.description.selector,
+        dpro.beijing.description)
+      .assert.elContainsText(beijing.elements.content.selector,
+        dpro.beijing.content);
   }
-
 });
