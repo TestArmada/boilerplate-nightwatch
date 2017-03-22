96a635ea8fc00fdc795be5774186447ddc457b1f
Lei Zhu <thunderzhulei@gmail.com>
2017-03-22 16:11:11 -0700

    add take element screenshot

--- tests/demo-first.js
+++ tests/demo-first.js
@@ -1,7 +1,7 @@
 var Test = require("../lib/example-base-test-class");
 
 module.exports = new Test({
-
+  "@tags": ["smoke"],
   "Load demo page": function (client) {
     client
       .url(this.getSiteURL() + "/demo-first");
@@ -9,6 +9,7 @@ module.exports = new Test({
 
   "Verify all cities": function (client) {
     client
+      .takeElScreenshot(".city:eq(2)", "a.png")
       .assert.elContainsText("#tokyo", "Tokyo")
       .assert.elContainsText(".city:eq(1) p:eq(1)", "Europe");
   }
