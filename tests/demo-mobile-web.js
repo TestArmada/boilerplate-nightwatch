feb9e2cccda7d9294baa8a202f776670bdde3690
Lei Zhu <thunderzhulei@gmail.com>
2017-05-09 11:00:49 -0700

    add mobile web demo

---
+++ tests/demo-mobile-web.js
@@ -0,0 +1,25 @@
+var Test = require("../lib/example-base-test-class");
+
+module.exports = new Test({
+
+  "Load goole page": function (client) {
+    client
+      .url("http://www.google.com");
+  },
+
+  "wait for lage load": function (client) {
+    client
+      .getEl("[name='q']")
+      .getEls("[name='q']", function (els) {
+        console.log(els);
+      });
+  },
+
+  "type search term": function (client) {
+    client
+      .setElValue("[name='q']", "xixixix")
+      .clearElValue("[name='q']")
+      .setElValue("[name='q']", "hahaha")
+      .clickEl("#tsbb");
+  }
+});
