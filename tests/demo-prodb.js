de202ac0756a76878fddf52f1ff3bd47d61e618e
Ruben Jimenez <rubenarismendi@gmail.com>
2019-12-23 19:09:30 -0400

    Add custom command to set dpro cookies

--- tests/demo-prodb.js
+++ tests/demo-prodb.js
@@ -1,15 +1,15 @@
 var Test = require("../lib/example-base-test-class");
-var config = require("dpro");
 
 module.exports = new Test({
   "@tags": ["prodb", "web"],
 
-  "Load demo page": function (client) {
-    client.url('https://www.walmart.com');
-    config.cookies.forEach(client.setCookie)
+  "It should set the prodenv cookie": function (client) {
     client
-      .refresh() // Reload to use the cookies
+      .setDproCookies('https://www.walmart.com')
       .waitForElementVisible('#global-search-input', 500)
+      .getCookie('SENV', function(response){
+        client.assert.equal(response.value, 'prodb');
+        return response.value;
+      })
   },
-
 });
